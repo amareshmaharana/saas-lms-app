25194cf9b105ca8e38d7abccf44b783fe0c86aad
Amaresh Maharana <amareshcoder@gmail.com>
2025-10-13 19:46:53 +0530

    Refactor page layout to display popular companions with CompanionCard components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,44 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to AI SaaS App</h1>
-      <Button>Click here!</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl'>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural network of the brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="science"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
