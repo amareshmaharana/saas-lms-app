1170c1909a04a7fc294e0a45e88a5f434f1c7f6f
Amaresh Maharana <amareshcoder@gmail.com>
2025-10-15 13:39:45 +0530

    feat: integrate Clerk authentication buttons in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
+
 import NavItems from './NavItems'
 
 const Navbar = () => {
@@ -12,9 +13,16 @@ const Navbar = () => {
                 </div>
             </Link>
 
-            <div className='flex items-center gap-8'>
+            <div className="flex items-center gap-8">
                 <NavItems />
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     )
