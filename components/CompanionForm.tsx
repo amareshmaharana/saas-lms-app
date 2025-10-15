987bf342ab8954524ee0af769af7bedce91ac9e7
Amaresh Maharana <amareshcoder@gmail.com>
2025-10-15 11:23:16 +0530

    feat: add CompanionForm component with validation and form handling

---
+++ components/CompanionForm.tsx
@@ -0,0 +1,215 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { subjects } from "@/constants";
+import { Textarea } from "./ui/textarea";
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Companion is required" }),
+  subject: z.string().min(1, { message: "Subject is required" }),
+  topic: z.string().min(1, { message: "Topic is required" }),
+  voice: z.string().min(1, { message: "Voice is required" }),
+  style: z.string().min(1, { message: "Style is required" }),
+  duration: z.number().min(1, { message: "Duration is required" }),
+});
+
+const CompanionForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      subject: '',
+      topic: '',
+      voice: '',
+      style: '',
+      duration: 15,
+    },
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+
+    // const companion = await createCompanion(values);
+
+    // if (companion) {
+    //   redirect(`/companions/${companion.id}`);
+    // } else {
+    //   console.log('Failed to create a companion');
+    //   redirect('/');
+    // }
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          {/* Companion Name */}
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Companion Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Companion Name" {...field} className="input" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Subject */}
+          <FormField
+            control={form.control}
+            name="subject"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Subject</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    value={field.value}
+                  >
+                    <SelectTrigger className="w-[180px] capitalize">
+                      <SelectValue placeholder="Select Subject" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      {subjects.map((subject) => (
+                        <SelectItem key={subject} value={subject} className="capitalize">
+                          {subject}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Topic */}
+          <FormField
+            control={form.control}
+            name="topic"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>What should the companion help with?</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Explain your topic" {...field} className="input" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Voice */}
+          <FormField
+            control={form.control}
+            name="voice"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Voice</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    value={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select a voice" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="Male">Male</SelectItem>
+                      <SelectItem value="Female">Female</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Style */}
+          <FormField
+            control={form.control}
+            name="style"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Style</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    value={field.value}
+                  >
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select a style" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem className="cursor-pointer" value="formal">Formal</SelectItem>
+                      <SelectItem className="cursor-pointer" value="casual">Casual</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Duration */}
+          <FormField
+            control={form.control}
+            name="duration"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Estimated session duration in mins</FormLabel>
+                <FormControl>
+                  <Input
+                    type="number"
+                    placeholder="12"
+                    {...field}
+                    value={field.value ?? ""}
+                    onChange={(e) => {
+                      const val = e.target.value;
+                      field.onChange(val === "" ? "" : Number(val));
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full cursor-pointer  mt-7" style={{ backgroundColor: "#FF4D00" }}>
+            Build your companion
+          </Button>
+        </form>
+      </Form>
+
+    </>
+  )
+}
+
+export default CompanionForm
