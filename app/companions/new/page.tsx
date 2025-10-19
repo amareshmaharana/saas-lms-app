564273fc12b446703d45d262836c204e516833c9
Amaresh Maharana <amareshcoder@gmail.com>
2025-10-19 16:21:54 +0530

    fix: add padding to main container in NewCompanion component

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -7,7 +7,7 @@ const NewCompanion = async () => {
   if (!userId) redirect("/sign-in");
 
   return (
-    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center pb-20">
       <article className="w-full gap-4 flex flex-col">
         <h1>Companion Builder</h1>
 
